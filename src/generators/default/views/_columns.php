6b5dc02b6d7852ca04edd2893c91d4e5bd73be09
aelsaidy <ahmad.elsaidy@hotmail.com>
2016-06-22 14:09:19 +0300

    adding language / support multi grid
    adding language / support multi grid

--- src/generators/default/views/_columns.php
+++ src/generators/default/views/_columns.php
@@ -14,6 +14,8 @@ $modelClass = StringHelper::basename($generator->modelClass);
 $urlParams = $generator->generateUrlParams();
 $nameAttribute = $generator->getNameAttribute();
 $actionParams = $generator->generateActionParams();
+$controlername = $generator->getControllerID();
+
 
 echo "<?php\n";
 
@@ -55,16 +57,16 @@ return [
         'dropdown' => false,
         'vAlign'=>'middle',
         'urlCreator' => function($action, $model, $key, $index) { 
-                return Url::to([$action,'<?=substr($actionParams,1)?>'=>$key]);
+                return Url::to(['/<?= $controlername ?>/' . $action,'<?=substr($actionParams,1)?>'=>$key]);
         },
-        'viewOptions'=>['role'=>'modal-remote','title'=>'View','data-toggle'=>'tooltip'],
-        'updateOptions'=>['role'=>'modal-remote','title'=>'Update', 'data-toggle'=>'tooltip'],
-        'deleteOptions'=>['role'=>'modal-remote','title'=>'Delete', 
-                          'data-confirm'=>false, 'data-method'=>false,// for overide yii data api
-                          'data-request-method'=>'post',
-                          'data-toggle'=>'tooltip',
-                          'data-confirm-title'=>'Are you sure?',
-                          'data-confirm-message'=>'Are you sure want to delete this item'], 
+        'viewOptions' => ['role' => 'modal-remote', 'title' => Yii::t('backend','View'), 'data-toggle' => 'tooltip'],
+                'updateOptions' => ['role' => 'modal-remote', 'title' => Yii::t('backend','Update'), 'data-toggle' => 'tooltip'],
+                'deleteOptions' => ['role' => 'modal-remote', 'title' => Yii::t('backend','Delete'),
+                    'data-confirm' => false, 'data-method' => false, // for overide yii data api
+                    'data-request-method' => 'post',
+                    'data-toggle' => 'tooltip',
+                    'data-confirm-title' => Yii::t('backend','Are you sure?'),
+                    'data-confirm-message' => Yii::t('backend','Are you sure want to delete this item')], 
     ],
 
 ];   
